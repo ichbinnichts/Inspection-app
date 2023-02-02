e0e75ac7e85aab7bda0215e6518240bfb7285755
Nathan Víctor de Faria <nathan.faria@inatel.br>
2023-02-02 10:48:04 -0300

    in inspection-api.service: agetInspectionList and addInspection

--- InspectionAPP/src/app/inspection-api.service.ts
+++ InspectionAPP/src/app/inspection-api.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InspectionApiService {
 
-  constructor() { }
+  readonly inspectionApiUrl = "https://localhost:7119/api";
+
+  constructor(private http:HttpClient) { }
+
+  getInspectionList():Observable<any[]> {
+    return this.http.get<any>(this.inspectionApiUrl + '/inspections');
+  }
+
+  addInspection(data:any){
+    return this.http.post(this.inspectionApiUrl + '/inspections', data);
+  }
 }

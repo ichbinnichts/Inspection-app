13fe7930bc8f7ac95cf5dc8a56ae48f98ad4e3c4
Nathan Víctor de Faria <nathan.faria@inatel.br>
2023-02-02 11:21:09 -0300

    Imports of show-inspection.component

--- InspectionAPP/src/app/inspection/show-inspection/show-inspection.component.ts
+++ InspectionAPP/src/app/inspection/show-inspection/show-inspection.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InspectionApiService } from 'src/app/inspection-api.service';
 
 @Component({
   selector: 'app-show-inspection',

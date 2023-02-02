78147d457e27892011713a254bd7902272b99f36
Nathan Víctor de Faria <nathan.faria@inatel.br>
2023-02-02 09:02:19 -0300

    add DataAnnotations.Schema to all models

--- InspectionAPI/Models/Status.cs
+++ InspectionAPI/Models/Status.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace InspectionAPI.Models
 {
     public class Status
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [StringLength(20)]

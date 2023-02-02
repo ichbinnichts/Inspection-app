3846945c0828808ab4d35bf21acd94b57190531a
Nathan Víctor de Faria <nathan.faria@inatel.br>
2023-02-02 09:46:27 -0300

    ng g c inspection with nested components such as show-inspection and add-edit-inspection

--- InspectionAPP/src/app/app.module.ts
+++ InspectionAPP/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InspectionComponent } from './inspection/inspection.component';
+import { ShowInspectionComponent } from './inspection/show-inspection/show-inspection.component';
+import { AddEditInspectionComponent } from './inspection/add-edit-inspection/add-edit-inspection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InspectionComponent,
+    ShowInspectionComponent,
+    AddEditInspectionComponent
   ],
   imports: [
     BrowserModule

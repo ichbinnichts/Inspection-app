d13ab1ea7ca68cd76469e5144ad2b70f473e765a
Nathan Víctor de Faria <nathan.faria@inatel.br>
2023-02-02 14:58:41 -0300

    Add the forms to the addEdit.component

--- InspectionAPP/src/app/inspection/add-edit-inspection/add-edit-inspection.component.ts
+++ InspectionAPP/src/app/inspection/add-edit-inspection/add-edit-inspection.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InspectionApiService } from 'src/app/inspection-api.service';
 
 @Component({
   selector: 'app-add-edit-inspection',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-edit-inspection.component.css']
 })
 export class AddEditInspectionComponent {
+  inspectionList$!:Observable<any[]>;
+  statusList$!:Observable<any[]>;
+  inspectionTypesList$!: Observable<any[]>;
+
+
+  constructor(private service:InspectionApiService){
+
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
+  @Input() inspection:any;
+  id: number = 0;
+  status: string = "";
+  comments: string = "";
+  inspectionTypeId!: number;
 
 }

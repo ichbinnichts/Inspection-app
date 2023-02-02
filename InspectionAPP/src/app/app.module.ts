dc8de491f7eab14392a19824bbe59b7c5c1a2983
Nathan Víctor de Faria <nathan.faria@inatel.br>
2023-02-02 10:31:18 -0300

    Import HttpClientModule FormsModule and ReactiveFormsModule

--- InspectionAPP/src/app/app.module.ts
+++ InspectionAPP/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
